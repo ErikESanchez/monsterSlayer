d3f010b678efb7fabe89441b17e5cc3d5fa45756
Erik Sanchez <eriksanchez7247@gmail.com>
2019-02-24 04:24:02 -0600

    Added a numbers to input

--- app.js
+++ app.js
@@ -3,5 +3,24 @@ new Vue({
     data: {
 
     },
-    
+    methods: {
+        attackDamage: function () {
+            let attackToMonster = Math.floor(Math.random() * -10);
+            let attackFromMonster = Math.floor(Math.random() * -7);
+            console.log(attackToMonster);
+            console.log(attackFromMonster);
+        },
+        specialAttackDamage: function () {
+            let specialAttackDamageToMonster = Math.floor(Math.random() * -15);
+            let specialAttackDamageFromMonster = Math.floor(Math.random() * -11);
+            console.log(specialAttackDamageToMonster);
+            console.log(specialAttackDamageFromMonster);
+        },
+        healPlayer: function () {
+            let healPlayer = Math.floor(Math.random() * 11);
+            let attackPlayerWhileHealing = Math.floor(Math.random() * -4);
+            console.log(healPlayer);
+            console.log(attackPlayerWhileHealing);
+        },
+    }
 });
