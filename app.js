9b0c8b92f3cc315b41b8ca824d1bc91cc96d092e
Erik Sanchez <eriksanchez7247@gmail.com>
2019-04-05 14:22:58 -0500

    Made progress bar work and added a computed

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 new Vue({
     el: '#app',
     data: {
-
+        playerHealth: 80,
+        monsterHealth: 80
     },
     methods: {
-        attackDamage: function () {
+        attackDamage() {
             let attackToMonster = Math.floor(Math.random() * -10);
             let attackFromMonster = Math.floor(Math.random() * -7);
-            console.log(attackToMonster);
-            console.log(attackFromMonster);
+            this.playerHealth >= 0;
+            this.playerHealth = this.playerHealth + attackFromMonster;
+            this.monsterHealth = this.monsterHealth + attackToMonster;
+            console.log("Player Health: " + this.playerHealth);
+            console.log("Monster Health: " + this.monsterHealth);
         },
         specialAttackDamage: function () {
             let specialAttackDamageToMonster = Math.floor(Math.random() * -15);
             let specialAttackDamageFromMonster = Math.floor(Math.random() * -11);
-            console.log(specialAttackDamageToMonster);
-            console.log(specialAttackDamageFromMonster);
+            this.playerHealth = this.playerHealth + specialAttackDamageFromMonster;
+            this.monsterHealth = this.monsterHealth + specialAttackDamageToMonster;
+            console.log("Player Health: " + this.playerHealth);
+            console.log("Monster Health: " + this.monsterHealth);
         },
         healPlayer: function () {
             let healPlayer = Math.floor(Math.random() * 11);
             let attackPlayerWhileHealing = Math.floor(Math.random() * -4);
-            console.log(healPlayer);
-            console.log(attackPlayerWhileHealing);
+            this.playerHealth = this.playerHealth + healPlayer + attackPlayerWhileHealing;
+            console.log("Player Health " + this.playerHealth);
         },
+        reset() {
+            this.playerHealth = 80,
+            this.monsterHealth = 80
+        }
+    },
+    computed: {
+        healthPlayer() {
+            return {width: this.playerHealth + '%'}
+        },
+        healthMonster() {
+            return {width: this.monsterHealth + '%'}
+        }
     }
+
 });
